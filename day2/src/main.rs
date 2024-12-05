a36ed6403c3c99f2eeb38e2996d052d3433bf51b
Miguel Martinez <martinez9142@gmail.com>
2024-12-04 18:59:55 -0600

    day 3 and 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,8 @@
 use std::{env::args, fs};
 
 fn main() {
-    part1();
+    // part1();
+    part2();
 }
 
 /*
@@ -61,4 +62,79 @@ fn part1() {
         }
     }
     println!("Number of Safe Reports: {}", safe_reports);
+}
+
+/*
+The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+
+The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
+
+Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+More of the above example's reports are now safe:
+
+    7 6 4 2 1: Safe without removing any level.
+    1 2 7 8 9: Unsafe regardless of which level is removed.
+    9 7 6 2 1: Unsafe regardless of which level is removed.
+    1 3 2 4 5: Safe by removing the second level, 3.
+    8 6 4 4 1: Safe by removing the third level, 4.
+    1 3 6 7 9: Safe without removing any level.
+
+Thanks to the Problem Dampener, 4 reports are actually safe!
+
+*/
+
+fn part2() {
+    let input = fs::read_to_string("src/input.txt");
+
+    let mut safe_reports = 0u32;
+    for line in input.unwrap().lines() {
+        if line.trim() == "" {
+            continue;
+        }
+
+        let report: Vec<i32> = line
+            .split_whitespace()
+            .map(|i| i.trim().parse::<i32>().unwrap())
+            .collect();
+
+        match is_safe(&report) {
+            (true, _) => safe_reports += 1,
+            (false, idx) => {
+                //do other stuff
+            }
+        }
+    }
+    println!("Safe Reports: {}", safe_reports);
+    //    println!("{:#?} is Safe: {} ", report, is_safe(&report));
+}
+
+fn is_safe(report: &[i32]) -> (bool, usize) {
+    let dif = report[0] - report[1];
+    let mut counter = 0usize;
+    for level in 0..report.len() - 1 {
+        match dif {
+            //decrasing
+            1..=3 => {
+                let temp = report[level] - report[level + 1];
+                if !(1..=3).contains(&temp) {
+                    return (false, level);
+                }
+            }
+            //increasing
+            -3..=-1 => {
+                let temp = report[level] - report[level + 1];
+                if !(-3..=-1).contains(&temp) {
+                    return (false, level);
+                }
+            }
+            _ => return (false, level),
+        }
+        counter += 1;
+    }
+    if counter != report.len() - 1 {
+        return (false, 0);
+    }
+
+    (true, 0)
 }
